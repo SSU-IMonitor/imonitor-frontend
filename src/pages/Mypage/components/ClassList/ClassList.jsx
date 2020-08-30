c212dd8c988a0438ad678dd90c7c37f2ca1e87a0
kjwook7522 <kjwook7522@gmail.com>
2020-08-31 01:30:38 +0900

    ClassList 섹션 디자인 완성

--- src/pages/Mypage/components/ClassList/ClassList.jsx
+++ src/pages/Mypage/components/ClassList/ClassList.jsx
@@ -2,7 +2,63 @@ import React from "react";
 import "./ClassList.css";
 
 function ClassList() {
-  return <section id="class-list">im classlist</section>;
+
+  return (
+    <section id="class-list">
+      <div className="header">
+        <h1>내 과목 목록</h1>
+        <button>수업 추가</button>
+      </div>
+      <ul className="list">
+        <li>
+          <h1>과목 중간고사</h1>
+          <h2>오범수</h2>
+        </li>
+        <li>
+          <h1>과목 중간고사</h1>
+          <h2>오범수</h2>
+        </li>
+        <li>
+          <h1>과목 중간고사</h1>
+          <h2>오범수</h2>
+        </li>
+        <li>
+          <h1>과목 중간고사</h1>
+          <h2>오범수</h2>
+        </li>
+        <li>
+          <h1>과목 중간고사</h1>
+          <h2>오범수</h2>
+        </li>
+        <li>
+          <h1>과목 중간고사</h1>
+          <h2>오범수</h2>
+        </li>
+        <li>
+          <h1>과목 중간고사</h1>
+          <h2>오범수</h2>
+        </li>
+        <li>
+          <h1>과목 중간고사</h1>
+          <h2>오범수</h2>
+        </li>
+        <li>
+          <h1>과목 중간고사</h1>
+          <h2>오범수</h2>
+        </li>
+        <li>
+          <h1>과목 중간고사</h1>
+          <h2>오범수</h2>
+        </li>
+        <div className="paging flex-row-center">
+          <span>-</span>
+          <span className="active">1</span>
+          <span>2</span>
+          <span>+</span>
+        </div>
+      </ul>
+    </section>
+  );
 }
 
 export default ClassList;
